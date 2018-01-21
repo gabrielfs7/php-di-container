58a6612c2a634841fa321bba8ade4a352505a795
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2018-01-21 18:18:37 +0100

    improving unit tests

--- tests/Unit/DiContainer/Cache/CreatorTest.php
+++ tests/Unit/DiContainer/Cache/CreatorTest.php
@@ -2,6 +2,7 @@
 
 namespace GSoares\Test\Unit\DiContainer\Dto\Decoder;
 
+use GSoares\DiContainer\Cache\CacheInterface;
 use GSoares\DiContainer\Cache\CompilerInterface;
 use GSoares\DiContainer\Cache\Creator;
 use GSoares\DiContainer\Cache\FileCreatorInterface;
@@ -18,24 +19,29 @@ class CreatorTest extends TestCase
     private $creator;
 
     /**
-     * @var MethodCreatorInterface
+     * @var MethodCreatorInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $methodCreator;
 
     /**
-     * @var FileCreatorInterface
+     * @var FileCreatorInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $fileCreator;
 
     /**
-     * @var ValidatorInterface
+     * @var ValidatorInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $validator;
 
     /**
-     * @var CompilerInterface
+     * @var CompilerInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $compiler;
+
+    /**
+     * @var CacheInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $cache;
 
     public function setUp()
     {
@@ -43,6 +49,7 @@ class CreatorTest extends TestCase
         $this->fileCreator = $this->createMock('\GSoares\DiContainer\Cache\FileCreatorInterface');
         $this->validator = $this->createMock('\GSoares\DiContainer\File\Validator\ValidatorInterface');
         $this->compiler = $this->createMock('\GSoares\DiContainer\Cache\CompilerInterface');
+        $this->cache = $this->createMock('\GSoares\DiContainer\Cache\CacheInterface');
 
         $this->creator = new Creator(
             $this->methodCreator,
@@ -66,6 +73,56 @@ class CreatorTest extends TestCase
      */
     public function testCreate()
     {
-        $this->markTestIncomplete();
+        $this->fileCreator
+            ->expects($this->once())
+            ->method('create')
+            ->with('path', 'services_parameters')
+            ->willReturn($this->cache);
+
+        $this->validator
+            ->expects($this->once())
+            ->method('validate')
+            ->with('file')
+            ->willReturn(null);
+
+        $this->validator
+            ->expects($this->once())
+            ->method('getServicesMap')
+            ->willReturn([]);
+
+        $this->validator
+            ->expects($this->once())
+            ->method('getParametersMap')
+            ->willReturn([]);
+
+        $this->methodCreator
+            ->expects($this->once())
+            ->method('createByServices')
+            ->with([])
+            ->willReturn(['services_']);
+
+        $this->methodCreator
+            ->expects($this->once())
+            ->method('createByParameters')
+            ->with([])
+            ->willReturn(['parameters']);
+
+        $this->fileCreator
+            ->expects($this->once())
+            ->method('getCacheFile')
+            ->willReturn('cache_file');
+
+        $this->compiler
+            ->expects($this->once())
+            ->method('compile')
+            ->with($this->cache, 'cache_file')
+            ->willReturn(null);
+
+        $this->cache
+            ->expects($this->once())
+            ->method('isCompiled')
+            ->willReturn(false);
+
+        $this->assertEquals($this->cache, $this->creator->enableCompile()->create('path', ['file']));
     }
 }
